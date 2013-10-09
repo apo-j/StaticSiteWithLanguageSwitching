bc5b960c031825ec7319dc11fae4e959f8d7e31f
Apo <jiyuny@yahoo.fr>
2013-10-10 00:47:01 +0200

    archi

--- dev/Scripts/global.js
+++ dev/Scripts/global.js
@@ -2,7 +2,8 @@ var vars = Namespace('com.joe.vars');
 var static = Namespace('com.joe.static');
 var types = Namespace('com.joe.types');
 var enums = Namespace('com.joe.enums');
-
+var pages = Namespace('com.joe.pages');
+var components = Namespace('com.joe.pages.components');
 //types
 types.languages = {
 	Chinese:'zh',
---
+++ dev/Scripts/page-components.js
@@ -0,0 +1,42 @@
+//definition of basic bricks
+	components.blocItem = function(id, on, title, src){
+			var self = this;
+			self.id = id;
+			self.on = ko.observable(on || false);
+			self.title = translator.translate(title);
+			self.src = src;
+	};
+	components.bloc = function(id, on, type, title, items){
+			var self = this;
+			self.id = id;
+			self.on = ko.observable(on || false);
+			self.type = type;
+			self.title = translator.translate(title);
+			self.items = items || [];
+	};
+	components.sideBar = function(id, on, items){
+			var self = this;
+			self.id = id;
+			self.on = ko.observable(on || false);
+			self.items = items || [];
+	};
+	components.section = function(id, on, title, body){
+			var self = this;
+			self.id = id;
+			self.on = ko.observable(on || false);
+			self.title = translator.translate(title);
+			self.body = translator.translate(body);
+	};
+	components.sections = function(items){
+			var self = this;
+			self.items = items || [];
+	};
+	components.page = function(param){
+			var self = this;
+			self.id = param.id;
+			self.on = ko.observable(param.on || false);
+			self.leftSideBar = param.leftSideBar || {on : false, items : []};
+			self.rightSideBar = param.rightSideBar || {on : false, items : []};;
+			self.title = translator.translate(param.title);
+			self.sections = param.sections || [];
+	};
